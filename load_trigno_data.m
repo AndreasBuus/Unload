33bed78d42ffd0b9ea45cc0dc070262571b5c826
Andreas Andersen <andersen.a.buus@gmail.com>
2024-04-19 09:57:18 +0200

    added structure and export

--- load_trigno_data.m
+++ load_trigno_data.m
@@ -3,26 +3,69 @@ clc; clear; close all;
 filefolder = struct;
 filefolder.tigno = "C:/Users/BuusA/Documents/Delsys/Trigno Discover/Unload_test/Andreas/2024-04-15/"; 
 
-left_soleus = 1; 
-Left_tibialis_anterior = 2; 
-rigth_soleus = 3; 
-right_tibialis = 4; 
+% variables 
+var = struct; 
 
-left_foot_inside_IMU = 5; 
-left_foot_inside_quaternion = 6; 
-left_foot_outside_quaternion = 7;
-left_ankel_quaternion = 8; 
+var.names.sensors = ["left_soleus", ...
+    "Left_tibialis_anterior", ...
+    "rigth_soleus", ...
+    "right_tibialis", ...
+    "left_foot_inside_IMU", ...
+    "left_foot_inside_quaternion", ...
+    "left_foot_outside_quaternion", ...
+    "left_ankel_quaternion", ...
+    "right_foot_outside_IMU", ...
+    "right_ankel_quaternion", ...
+    "right_foot_inside_quaternion", ...
+    "head_IMU"]; 
 
-right_foot_outside_IMU = 9; 
-right_ankel_quaternion = 10; 
-right_foot_inside_quaternion = 11; 
+var.names.IMU = ["time (s)", "acc x (G)", "acc y (G)", "acc z (G) ", "gyro X (deg/s)", "gyroY (deg/s)", "gyroZ (deg/s)"]; 
+var.names.quaternion = ["time (s)", "oriW (Quaternion)", "oriX (Quaternion)", "oriY (Quaternion)","oriZ (Quaternion)" ]; 
+var.names.EMG = ["time (s)", "EMG (mV)"]; 
 
-head_IMU = 12; 
+var.time = 1; 
+
+% Accelerometer
+var.acc.x = 2; 
+var.acc.y = 3;
+var.acc.z = 4;
+
+% Gyro
+var.gyro.x = 5; 
+var.gyro.y = 6; 
+var.gyro.z = 7;
+
+% Quaternion
+var.quat.w = 2; 
+var.quat.x = 3; 
+var.quat.y = 4; 
+var.quat.z = 5; 
+
+
+% EMG sensors
+var.left_soleus = 1; 
+var.Left_tibialis_anterior = 2; 
+var.rigth_soleus = 3; 
+var.right_tibialis = 4; 
+
+% Left sensors
+var.left_foot_inside_IMU = 5; 
+var.left_foot_inside_quaternion = 6; 
+var.left_foot_outside_quaternion = 7;
+var.left_ankel_quaternion = 8; 
+
+% Right sensors
+var.right_foot_outside_IMU = 9; 
+var.right_ankel_quaternion = 10; 
+var.right_foot_inside_quaternion = 11; 
+
+% Head sensor
+var.head_IMU = 12; 
 
 for sweep = 1:23
 
     % Print progress message with backspace characters to overwrite the previous line
-    progress = round(sweep / 23 * 100);                        % Calculate progress percentage
+    progress = round(sweep / 23 * 100);                 % Calculate progress percentage
     progressMessage = sprintf('%d pct', progress);      % Format the progress message
     deleteCount = numel(progressMessage) + 1; %         % Determine the number of characters to delete 
     fprintf([repmat('\b', 1, deleteCount) '%s'], progressMessage);
@@ -55,34 +98,25 @@ for sweep = 1:23
     y_qua = isnan(x_qua(:,1)); 
     
     % Store EMG data
-    data_trigno{sweep,left_soleus}            = data(~y_emg, 1:2); 
-    data_trigno{sweep,Left_tibialis_anterior} = data(~y_emg, 3:4); 
-    data_trigno{sweep,rigth_soleus}           = data(~y_emg, 5:6); 
-    data_trigno{sweep,right_tibialis}         = data(~y_emg, 7:8); 
+    var.trigno{sweep,var.left_soleus}            = data(~y_emg, 1:2); 
+    var.trigno{sweep,var.Left_tibialis_anterior} = data(~y_emg, 3:4); 
+    var.trigno{sweep,var.rigth_soleus}           = data(~y_emg, 5:6); 
+    var.trigno{sweep,var.right_tibialis}         = data(~y_emg, 7:8); 
     
     % Store IMU
-    data_trigno{sweep, left_foot_inside_IMU}    = data(~y_imu, [9,10:2:20]);
-    data_trigno{sweep, right_foot_outside_IMU}  = data(~y_imu, [51,52:2:62]);
-    data_trigno{sweep, head_IMU}                = data(~y_imu, [83,84:2:94]);
+    var.trigno{sweep, var.left_foot_inside_IMU}    = data(~y_imu, [9,10:2:20]);
+    var.trigno{sweep, var.right_foot_outside_IMU}  = data(~y_imu, [51,52:2:62]);
+    var.trigno{sweep, var.head_IMU}                = data(~y_imu, [83,84:2:94]);
     
     % Store quaterion
-    data_trigno{sweep, left_foot_inside_quaternion}     = data(~y_qua, [23,24:2:30]);
-    data_trigno{sweep, left_foot_outside_quaternion}    = data(~y_qua, [33,34:2:40]);
-    data_trigno{sweep, left_ankel_quaternion}           = data(~y_qua, [43,44:2:50]);
-    data_trigno{sweep, right_ankel_quaternion}          = data(~y_qua, [65,66:2:72]);
-    data_trigno{sweep, right_foot_inside_quaternion}    = data(~y_qua, [75,76:2:82]);
+    var.trigno{sweep, var.left_foot_inside_quaternion}     = data(~y_qua, [23,24:2:30]);
+    var.trigno{sweep, var.left_foot_outside_quaternion}    = data(~y_qua, [33,34:2:40]);
+    var.trigno{sweep, var.left_ankel_quaternion}           = data(~y_qua, [43,44:2:50]);
+    var.trigno{sweep, var.right_ankel_quaternion}          = data(~y_qua, [65,66:2:72]);
+    var.trigno{sweep, var.right_foot_inside_quaternion}    = data(~y_qua, [75,76:2:82]);
 end 
 deleteCount = numel(progressMessage) + 1; %         % Determine the number of characters to delete 
 fprintf([repmat('\b', 1, deleteCount) '%s'], "done");
 
-% accX = 1; accY = 2; accZ = 3; 
-% gyroX = 4; gyroY = 5; gyroZ = 6; 
-% 
-% oriW = 1; oriX = 2; oriY = 3; oriZ = 4; 
-% 
-% data_tigno = cell(10,4); 
-% shoeAG = 1; 
-% shoeO = 2; 
-% ankelAG = 3;
-% ankelO = 4; 
-% time = 5; 
+
+save('var')
