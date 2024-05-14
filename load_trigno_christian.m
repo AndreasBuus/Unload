b5627e98f2bc7fd917a318636c0be849c534383c
Andreas Andersen <andersen.a.buus@gmail.com>
2024-05-14 09:42:03 +0200

    First attemp at unload subject 3

--- load_trigno_christian.m
+++ load_trigno_christian.m
@@ -1,17 +1,12 @@
 clc; clear; close all;
 
 filefolder = struct;
-filefolder.tigno = "C:/Users/BuusA/Documents/Delsys/Trigno Discover/unload_50and350/andreas/2024-05-01/"; 
+filefolder.tigno = "C:/Users/BuusA/OneDrive/Documents/Delsys/Trigno Discover/Unload/Christian/2024-05-07"; 
+
+filename = "/trial3__"; 
 
 % variables 
 trigno = struct; 
-
-trigno.names.sensors = ["left_foot_QUA", ...
-    "left_ankel_QUA", ... 
-    "left_shoe_IMU", ... 
-    "left_soleus_EMG", ...
-    "left_tibialis_anterior_EMG", ...
-    "head_IMU"]; 
 
 trigno.names.IMU = ["time (s)", "acc x (G)", "acc y (G)", "acc z (G) ", "gyro X (deg/s)", "gyroY (deg/s)", "gyroZ (deg/s)"]; 
 trigno.names.quaternion = ["time (s)", "oriW (Quaternion)", "oriX (Quaternion)", "oriY (Quaternion)","oriZ (Quaternion)" ]; 
@@ -35,21 +30,23 @@ trigno.quat.x = 3;
 trigno.quat.y = 4; 
 trigno.quat.z = 5; 
 
-% Kinomatik sensors
-trigno.left_foot_quaternion = 1; 
-trigno.left_ankel_quaternion = 2; 
-trigno.left_foot_IMU = 3; 
+% sensor names
+trigno.l_sol = 1; 
+trigno.l_ta = 2; 
+trigno.r_sol = 3; 
+trigno.r_ta = 4; 
+trigno.l_shoe = 5; 
+trigno.l_ankel = 6; 
+trigno.l_shoe_imu = 7; 
+trigno.head_imu = 8; 
+trigno.l_qf = 9; 
+trigno.l_bf = 10; 
+trigno.l_thigh = 11; 
 
-% EMG sensors
-trigno.left_soleus = 4; 
-trigno.left_tibialis_anterior = 5; 
 
-% Head sensor
-trigno.head_IMU = 6; 
- 
 % Exclude sweeps;
-sweeps_recorded = 1:121; 
-excluded_sweeps = [12,13,14,15,16,40,72,93, 44,48,103]; 
+sweeps_recorded = 1:103; 
+excluded_sweeps = [21]; 
 sweeps_recorded(excluded_sweeps) = [];
 
 
@@ -61,7 +58,7 @@ for i = 1:length(sweeps_recorded)
     deleteCount = numel(progressMessage) + 1; %         % Determine the number of characters to delete 
     fprintf([repmat('\b', 1, deleteCount) '%s'], progressMessage);
 
-    filenames = "test1__"+sweeps_recorded(i)+"/test1__"+sweeps_recorded(i)+".csv";
+    filenames = filename + sweeps_recorded(i) + filename+sweeps_recorded(i)+".csv";
     filepath = filefolder.tigno + filenames; 
         
     clear  opts
@@ -80,30 +77,34 @@ for i = 1:length(sweeps_recorded)
     clear x_qua y_qua
     x_qua = data(:, 3); 
     y_qua = isnan(x_qua(:,1));     % identify non-nan files
-    trigno.data{i, trigno.left_foot_quaternion}  = data(~y_qua, 1+[2,3,5,7,9]);
-    trigno.data{i, trigno.left_ankel_quaternion} = data(~y_qua, 11+[2,3,5,7,9]);
+    trigno.data{i, trigno.l_shoe}  = data(~y_qua, 9+[2,3,5,7,9]);
+    trigno.data{i, trigno.l_ankel} = data(~y_qua, 19+[2,3,5,7,9]);
+    trigno.data{i, trigno.l_thigh} = data(~y_qua, 57+[2,3,5,7,9]);
 
     % Store IMU
     clear x_imu y_imu
     x_imu = data(:, 21); 
     y_imu = isnan(x_imu(:,1));     % identify non-nan files
-    trigno.data{i, trigno.left_foot_IMU}         = data(~y_imu, 21+[0,1:2:11]);
-    trigno.data{i, trigno.head_IMU}              = data(~y_imu, 37+[0,1:2:11]);
+    trigno.data{i, trigno.l_shoe_imu} = data(~y_imu, 29+[0,1:2:11]);
+    trigno.data{i, trigno.head_imu}   = data(~y_imu, 41+[0,1:2:11]);
 
     % Store EMG data
     clear x_emg y_emg
     x_emg = data(:, 33); 
     y_emg = isnan(x_emg(:,1));     % identify non-nan files
-    trigno.data{i,trigno.left_soleus}            = data(~y_emg, 33+[0,1]); 
-    trigno.data{i,trigno.left_tibialis_anterior} = data(~y_emg, 35+[0,1]); 
+    trigno.data{i,trigno.l_sol} = data(~y_emg, 1+[0,1]); 
+    trigno.data{i,trigno.l_ta}  = data(~y_emg, 3+[0,1]); 
+    trigno.data{i,trigno.r_sol} = data(~y_emg, 5+[0,1]); 
+    trigno.data{i,trigno.r_ta}  = data(~y_emg, 7+[0,1]); 
+    trigno.data{i,trigno.l_qf}  = data(~y_emg, 53+[0,1]); 
+    trigno.data{i,trigno.l_bf}  = data(~y_emg, 55+[0,1]); 
+
 
 end 
+
 deleteCount = numel(progressMessage) + 1; %         % Determine the number of characters to delete 
 fprintf([repmat('\b', 1, deleteCount) '%s'], "done");
 
-
-
-
-save('data_trigno_andreas_121', "trigno")
+save('trigno_christian_data', "trigno")
 
 
---
+++ unload_christian.m
@@ -0,0 +1,276 @@
+clc; clear; close all; 
+subject = "Andreas";
+
+load('trigno_christian_data.mat')  % load data from delsys trigno 
+load("kick_christian.mat")        % load data from Mr.Kick  
+
+
+%% classes
+
+classes{1} = find(kick.swp_class == 0);
+classes{2} = find(kick.swp_class == 1);
+classes{3} = find(kick.swp_class == 2);
+
+included_sweeps = 1:length(kick.swp_class); 
+
+
+%% Align Kick and Trigno 
+
+% Find trigger position 
+x_axis = linspace(-pretrig, 10 - pretrig,  20000 ); % from kick
+for sweep = 1:length(included_sweeps)
+    close all; 
+    %disp(sweep)
+    y_forceG = kick.data{sweep, kick.trig_level}; 
+
+    threshold = 1; 
+    %threshold = mean(y_forceG(end/2:end)) + abs(mean(y_forceG(end/2:end))*0.50); 
+
+    trig_positive = find(y_forceG > threshold == 1);
+    trig_position_idx = trig_positive(1); % samples
+    trig_position_sec(sweep) = x_axis(trig_position_idx);  % sec
+
+    % figure; hold on 
+    % plot(x_axis, y_forceG(:))
+    % plot([x_axis(1), x_axis(end)],[threshold, threshold])
+    % plot( [x_axis(trig_position_idx),x_axis(trig_position_idx)], [min(y_forceG),max(y_forceG)])
+    % pause
+end
+
+figure; hold on 
+plot(x_axis, y_forceG(:))
+plot([x_axis(1), x_axis(end)],[threshold, threshold])
+plot( [x_axis(trig_position_idx),x_axis(trig_position_idx)], [min(y_forceG),max(y_forceG)])
+
+%% Acquisition Set-Up
+acq = struct; % acq = acquisition
+
+acq.sweeps = length(included_sweeps);
+
+% frequency
+acq.fs.O    = 222.222;   % Samples per second [Hz]
+acq.fs.EMG  = 2148.1481; % Samples per second [Hz]
+acq.fs.AG   = 370.3704;  % Samples per second [Hz]
+acq.fs.Kick = 2000;      % Samples per second [Hz]
+
+% periode dt
+acq.dt.O  =  1/acq.fs.O;      % Seconds per sample [sec]
+acq.dt.EMG  =  1/acq.fs.EMG;  % Seconds per sample [sec]
+acq.dt.AG =  1/acq.fs.AG;     % Seconds per sample [sec]
+acq.dt.Kick =  1/acq.fs.Kick; % Seconds per sample [sec]
+
+% pre-tiggrer
+switch subject
+    case "Andreas"
+    acq.pre_trig.kick = 4;  % [sec]
+    case "Pegah"
+    acq.pre_trig.kick = 6;  % [sec]
+    otherwise
+    error("Not a valid obtions for subject input")
+end
+acq.pre_trig.AG  = trig_position_sec;    % [sec]
+acq.pre_trig.O   = trig_position_sec;    % [sec]
+acq.pre_trig.EMG = trig_position_sec;    % [sec]
+
+for sweep = 1:acq.sweeps
+    acq.N.AG(sweep)   = length(trigno.data{sweep,trigno.left_foot_IMU}(:,2));
+    acq.N.O(sweep)    = length(trigno.data{sweep,trigno.left_foot_quaternion}(:,2));
+    acq.N.EMG(sweep)  = length(trigno.data{sweep,trigno.left_soleus}(:,2));
+    acq.N.Kick(sweep) = length(kick.data{sweep,1});
+
+    acq.time_axis.AG{sweep}   = trigno.data{sweep,trigno.left_foot_IMU}(:,1) + trig_position_sec(sweep);
+    acq.time_axis.ori{sweep}  = trigno.data{sweep,trigno.left_foot_quaternion}(:,1) + trig_position_sec(sweep);
+    acq.time_axis.EMG{sweep}  = trigno.data{sweep,trigno.left_soleus}(:,1) + trig_position_sec(sweep);
+    acq.time_axis.kick{sweep} = linspace(-acq.pre_trig.kick, acq.N.Kick(sweep)*acq.dt.Kick - acq.pre_trig.kick, acq.N.Kick(sweep)); 
+end
+
+
+
+%% Degrees
+ 
+for sweep = 1:acq.sweeps
+    % ankel
+    w = trigno.data{sweep, trigno.left_ankel_quaternion}(:, trigno.quat.w);
+    x = trigno.data{sweep, trigno.left_ankel_quaternion}(:, trigno.quat.x);
+    y = trigno.data{sweep, trigno.left_ankel_quaternion}(:, trigno.quat.y);
+    z = trigno.data{sweep, trigno.left_ankel_quaternion}(:, trigno.quat.z);
+    left_ankel = quaternion(w,x,y,z); 
+    
+    % foot
+    w = trigno.data{sweep, trigno.left_foot_quaternion}(:, trigno.quat.w);
+    x = trigno.data{sweep, trigno.left_foot_quaternion}(:, trigno.quat.x);
+    y = trigno.data{sweep, trigno.left_foot_quaternion}(:, trigno.quat.y);
+    z = trigno.data{sweep, trigno.left_foot_quaternion}(:, trigno.quat.z);
+    left_shoe = quaternion(w,x,y,z); 
+    
+    % quaternion -> euler
+    e_shoe = rad2deg(euler(left_shoe, 'ZYX', 'frame')); 
+    e_ankel = rad2deg(euler(left_ankel, 'ZYX', 'frame')); 
+    
+    % difference
+    data.left_ankel{sweep} = - e_ankel(:,2) - e_shoe(:,2); 
+end
+
+
+
+%% Find average 
+
+for c = 1:3
+
+    % Find largest start value (time) and smallest end value (time)
+    first_time_value_EMG = []; last_time_value_EMG = [];     % EMG 
+    first_time_value_ori = []; last_time_value_ori = [];     % Quaterion 
+    for i = 1:length(classes{c})
+        % EMG
+        first_time_value_EMG(i) = acq.time_axis.EMG{classes{c}(i)}(1); 
+        last_time_value_EMG(i)  = acq.time_axis.EMG{classes{c}(i)}(end); 
+        % quaterion
+        first_time_value_ori(i) = acq.time_axis.ori{classes{c}(i)}(1); 
+        last_time_value_ori(i)  = acq.time_axis.ori{classes{c}(i)}(end); 
+    end 
+    start_time_EMG = max(first_time_value_EMG); % EMG start
+    end_time_EMG = min(last_time_value_EMG);    % EMG end 
+    start_time_ori = max(first_time_value_ori); % quaterion start
+    end_time_ori = min(last_time_value_ori);    % quaterion end 
+
+
+    % Find closest idx that match found values (time)
+    closest_idx_start_EMG = []; closest_idx_end_EMG = []; % EMG
+    closest_idx_start_ori = []; closest_idx_end_ori = []; % quaterion
+    for i = 1:length(classes{c})
+        % EMG
+        [~,closest_idx_start_EMG(i)] = min(abs(acq.time_axis.EMG{classes{c}(i)}+abs(start_time_EMG)));
+        [~,closest_idx_end_EMG(i)]   = min(abs(acq.time_axis.EMG{classes{c}(i)}-end_time_EMG)); 
+        % quaterion
+        [~,closest_idx_start_ori(i)] = min(abs(acq.time_axis.ori{classes{c}(i)}+abs(start_time_ori)));
+        [~,closest_idx_end_ori(i)]   = min(abs(acq.time_axis.ori{classes{c}(i)}-end_time_ori)); 
+    end 
+    diff_between_EMG = []; 
+    diff_between_EMG = closest_idx_end_EMG(:) - closest_idx_start_EMG(:); 
+    diff_between_ori = []; 
+    diff_between_ori = closest_idx_end_ori(:) - closest_idx_start_ori(:);
+
+
+    %check for error
+    if or((max(diff(unique(diff_between_EMG))) > 1),  numel(unique(diff_between_EMG)) > 2)
+        error_message =  "Error in EMG mean. Big difference between included indexs sizes";
+        errordlg(error_message , 'Error');
+        error(error_message)
+    end
+    if or((max(diff(unique(diff_between_ori))) > 1),  numel(unique(diff_between_ori)) > 2)
+        error_message =  "Error in ankel mean. Big difference between included indexs sizes";
+        errordlg(error_message , 'Error');
+        error(error_message)
+    end
+
+
+    % remove 1 sample diffrence
+    for i = find(diff_between_EMG == max(diff_between_EMG))
+        closest_idx_end_EMG(i) = closest_idx_end_EMG(i) - 1; 
+    end 
+    for i = find(diff_between_ori == max(diff_between_ori))
+        closest_idx_end_ori(i) = closest_idx_end_ori(i) - 1; 
+    end
+
+    % create matrix to find mean 
+    left_sol_matrix = []; left_ta_matrix = []; 
+    left_palm = []; left_hell = []; 
+    left_ankel_matrix = []; stair_pos = []; stair_force = []; 
+
+    for i = 1:length(classes{c})
+        % level ground
+        left_sol_matrix(i,:) = abs(detrend(trigno.data{classes{c}(i),trigno.left_soleus}(closest_idx_start_EMG(i):closest_idx_end_EMG(i),2))); 
+        left_ta_matrix(i,:) = abs(detrend(trigno.data{classes{c}(i),trigno.left_tibialis_anterior}(closest_idx_start_EMG(i):closest_idx_end_EMG(i),2))); 
+        left_ankel_matrix(i,:) = data.left_ankel{classes{c}(i)}(closest_idx_start_ori(i):closest_idx_end_ori(i));
+        
+        stair_pos(i,:) = kick.data{classes{c}(i),kick.pos_stair}; 
+        stair_force(i,:) = kick.data{classes{c}(i),kick.force_stair}; 
+
+        left_palm(i,:) = kick.data{classes{c}(i),kick.FSR_palm}; 
+        left_hell(i,:) = kick.data{classes{c}(i),kick.FSR_hell}; 
+    end 
+    data.left_sol_class{c} = mean(left_sol_matrix,1); 
+    data.left_ta_class{c} = mean(left_ta_matrix, 1);
+    data.left_palm{c} = mean(left_palm,1); 
+    data.left_hell{c} = mean(left_hell,1); 
+    data.left_ankel_mean{c} = mean(left_ankel_matrix,1); 
+    data.stair{c} = mean(stair_pos,1);
+    data.stair_force{c} = mean(stair_force,1);
+    
+    % new time axis
+    acq.time_axis.EMG_mean_class{c} = acq.time_axis.EMG{classes{c}(1)}(closest_idx_start_EMG(1):closest_idx_end_EMG(1));
+    acq.time_axis.ori_mean_class{c} = acq.time_axis.ori{classes{c}(1)}(closest_idx_start_ori(1):closest_idx_end_ori(1));
+end
+
+%% plot
+
+legend_name = ["Control [n="+num2str(numel(classes{1}))+"]",...
+    "100 ms unload [n="+num2str(numel(classes{2}))+"]", ...
+    "200 ms unload [n="+num2str(numel(classes{3}))+"]"]; 
+
+xlimits_ground = [-4 6.5]; 
+xlimits_ground = [-1 1]; 
+%xlimits_ground = [0.35 0.55]
+%xlimits_ground = [0.25 0.55]
+
+%for sweep = classes{1}
+%    close all;
+%    disp(sweep)
+
+figure;
+hold on;
+
+subplot(6,1,1); hold on; 
+title("Left Soleus")
+%plot(acq.time_axis.EMG{sweep}, trigno.data{sweep,trigno.left_soleus}(:,2), 'Color',"yellow",'LineWidth',1) 
+plot(acq.time_axis.EMG_mean_class{1}, data.left_sol_class{1}, 'Color',[0.8 0.8 0.8],'LineWidth',2) 
+plot(acq.time_axis.EMG_mean_class{2}, data.left_sol_class{2}, 'Color',"red",'LineWidth',1) 
+plot(acq.time_axis.EMG_mean_class{3}, data.left_sol_class{3}, 'Color',"blue",'LineWidth',1) 
+xlim(xlimits_ground)
+legend(legend_name); 
+
+subplot(6,1,2); hold on; 
+title("Left Tibialis Anterior")
+% plot(acq.time_axis.EMG{sweep}, trigno.data{sweep,trigno.left_tibialis_anterior}(:,2), 'Color',"yellow",'LineWidth',1) 
+plot(acq.time_axis.EMG_mean_class{1}, data.left_ta_class{1}, 'Color',[0.8 0.8 0.8],'LineWidth',2) 
+plot(acq.time_axis.EMG_mean_class{2}, data.left_ta_class{2}, 'Color',"red",'LineWidth',1) 
+plot(acq.time_axis.EMG_mean_class{3}, data.left_ta_class{3}, 'Color',"blue",'LineWidth',1) 
+xlim(xlimits_ground)
+
+
+subplot(6,1,3); hold on; 
+title("left ankel position")
+plot(acq.time_axis.ori_mean_class{1}, data.left_ankel_mean{1}, 'Color',[0.8 0.8 0.8],'LineWidth',2) 
+plot(acq.time_axis.ori_mean_class{2}, data.left_ankel_mean{2}, 'Color',"red",'LineWidth',1) 
+plot(acq.time_axis.ori_mean_class{3}, data.left_ankel_mean{3}, 'Color',"blue",'LineWidth',1) 
+xlim(xlimits_ground)
+
+
+subplot(6,1,4); hold on; 
+title("Stair step position [cm]")
+plot(acq.time_axis.kick{1}, data.stair{1}, 'Color',[0.8 0.8 0.8],'LineWidth',2) 
+plot(acq.time_axis.kick{1}, data.stair{2}, 'Color',"red",'LineWidth',1) 
+plot(acq.time_axis.kick{1}, data.stair{3}, 'Color',"blue",'LineWidth',1) 
+xlim(xlimits_ground)
+
+
+subplot(6,1,5); hold on; 
+title("stair force [z]")
+plot(acq.time_axis.kick{1}, data.stair_force{1}, 'Color',[0.8 0.8 0.8],'LineWidth',2) 
+plot(acq.time_axis.kick{1}, data.stair_force{2}, 'Color',"red",'LineWidth',1) 
+plot(acq.time_axis.kick{1}, data.stair_force{3}, 'Color',"blue",'LineWidth',1) 
+xlim(xlimits_ground)
+
+
+subplot(6,1,6); hold on; 
+title("Foot switch (palm)")
+plot(acq.time_axis.kick{1}, data.left_palm{1}, 'Color',[0.8 0.8 0.8],'LineWidth',2) 
+plot(acq.time_axis.kick{1}, data.left_palm{2}, 'Color',"red",'LineWidth',1) 
+plot(acq.time_axis.kick{1}, data.left_palm{3}, 'Color',"blue",'LineWidth',1) 
+xlim(xlimits_ground)
+
+%pause
+%end
+
+
+
